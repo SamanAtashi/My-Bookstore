bfdafc788c0f1d59bc5c08b8a98052422fee82cf
SamanAtashi <milady00126@gmail.com>
2021-09-02 06:11:35 +0300

    Add categories.js and routes to A[pp.js

---
+++ src/components/Categories.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Categories = () => {
+	return <p>Under construction</p>;
+};
+
+export default Categories;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );

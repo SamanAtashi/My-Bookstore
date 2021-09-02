cfda929b7da672b444c5fb401be3e100865799c0
SamanAtashi <milady00126@gmail.com>
2021-09-02 06:16:52 +0300

    Add linters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
-const App = () => {
-	return (
-		<div>
-        <h1>Welcome to BookStore</h1>
-			<Router>
-				<Link to="/books">Books</Link>
-				<Link to="/categories">Categories</Link>
-				<Switch>
-					<Route path="/books" >
-						<Books />
-					</Route>
-					<Route path="/categories" >
-						<Categories />
-					</Route>
-				</Switch>
-			</Router>
-		</div>
-	);
-};
+const App = () => (
+  <div>
+    <h1>Welcome to BookStore</h1>
+    <Router>
+      <Link to="/books">Books</Link>
+      <Link to="/categories">Categories</Link>
+      <Switch>
+        <Route path="/books">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
 export default App;

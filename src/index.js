6f009e16292cd25a12dd534d13432295955da843
SamanAtashi <milady00126@gmail.com>
2021-09-02 05:46:29 +0300

    Add structural files and folders for redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>hello</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
